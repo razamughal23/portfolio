2b8d12af0715192c3504e0f17ab2d54934beb9a1
Raza Mughal <razasabir23outlook.com>
2024-04-21 21:43:44 +0400

    new code added

---
+++ constants/constants.js
@@ -0,0 +1,64 @@
+export const NAME = "Raza Sabir";
+
+export const DESIGNATION = `Front End Develper | React Js | Next Js | MUI`;
+
+export const DETAILS = {
+  Residence: "Dubai, UAE",
+  Nationality: "Pakistani",
+  City: "Lahore",
+  Age: "26",
+};
+
+export const SKILLS = [
+  {
+    title: "React Js Developer",
+    level: "95%",
+  },
+  {
+    title: "Next JS Developer",
+    level: "95%",
+  },
+  {
+    title: "Javascript / Typescript",
+    level: "95%",
+  },
+  {
+    title: "Material UI",
+    level: "100%",
+  },
+  {
+    title: "GitHub",
+    level: "95%",
+  },
+  {
+    title: "Backend Developer",
+    level: "50%",
+  },
+];
+
+export const TECH_STACK = [
+  "JavaScript",
+  "ReactJS",
+  "NextJS",
+  "TypeScript",
+  "CSS",
+  "SCSS",
+  "TailwindCSS",
+  "Material UI",
+  "GraphQL API",
+  "Git",
+  "GitHub",
+  "Jira",
+  "Figma",
+  "Adobe XD",
+  "Coral Draw",
+];
+
+export const CONTACTS = {
+  EMAIL: "razasabir23@icloud.com",
+  PHONE: "+92 320 0430489 / +971 54 3207229",
+};
+export const SOCIAL_LINKS = {
+  GITHUB: "https://github.com/razamughal23",
+  LINKEDIN: "https://www.linkedin.com/in/raza-mughal-developer/",
+};
